2158dc7447f6e424d0b63958e3a3be03af85ae38
Roman Rudi <roman.rudi@outlook.com>
2022-10-07 18:04:39 +0300

    Third commit

--- src/main/java/ru/roman/Main.java
+++ src/main/java/ru/roman/Main.java
@@ -3,6 +3,6 @@ package ru.roman;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! ");
-        System.out.println("Hello world! ");
+        System.out.println("Hello world, man! ");
     }
 }
